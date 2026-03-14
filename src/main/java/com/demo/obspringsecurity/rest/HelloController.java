21e5b0cb909f883bd316bc4dc2cb64ceea6ec12b
gfCrova <giancrova.cl5@gmail.com>
2026-03-13 23:50:20 -0300

    Change / for /api/hello

--- src/main/java/com/demo/obspringsecurity/rest/HelloController.java
+++ src/main/java/com/demo/obspringsecurity/rest/HelloController.java
@@ -1,17 +1,18 @@
 package com.demo.obspringsecurity.rest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    private final Logger log = LoggerFactory.getLogger(HelloController.class);
-
     @GetMapping("/")
     public String index() {
+        return "Hola, Bienvenido a la app de User Login!";
+    }
+
+    @GetMapping("/api/hello")
+    public String hello() {
         return """
                 <!doctype html>
                 <html lang="en">
@@ -22,16 +23,10 @@ public class HelloController {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
                   </head>
                   <body>
-                    <h1>Haz Accedido! Bienvenido a Spring Security...</h1>
+                    <h1 align="center">Haz Accedido! Bienvenido a Spring Security...</h1>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
                   </body>
                 </html>
                 """;
-    }
-
-    @GetMapping("/api/hello")
-    public String hello() {
-        log.info("Executing hello world method from logger");
-        return "Hola, Bienvenido a la app de User Login!";
     }
 }
